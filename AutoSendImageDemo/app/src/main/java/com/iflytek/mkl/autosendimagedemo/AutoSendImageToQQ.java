e7313fcd12ee8dc1329bd3f0fdebae3e1cf644b9
mkl <makunle@qq.com>
2017-08-23 23:56:22 +0800

    auto send is ok if not speek to a man before, can't find it [problem] can interuput this chain [need know if it's IME event]

--- AutoSendImageDemo/app/src/main/java/com/iflytek/mkl/autosendimagedemo/AutoSendImageToQQ.java
+++ AutoSendImageDemo/app/src/main/java/com/iflytek/mkl/autosendimagedemo/AutoSendImageToQQ.java
@@ -2,6 +2,7 @@ package com.iflytek.mkl.autosendimagedemo;
 
 import android.accessibilityservice.AccessibilityService;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
@@ -26,75 +27,86 @@ public class AutoSendImageToQQ extends AccessibilityService {
     private static final String QQ_COMMIT_DIALOG_ID = "com.tencent.mobileqq:id/dialogTitle";
     private static final String QQ_COMMIT_DIALOG_YES_ID = "com.tencent.mobileqq:id/dialogRightBtn";
 
-    private static final String QQ_PACKAGE_NAME = "com.tencent.mobileqq";
-    private static final String IME_PACKAGE_NAME = "com.iflytek.inputmethod";
-
     private static final String TAG = "AutoSendImageToQQ";
-
-    private int nowState = 0;
-    private String chaterName = null;
 
     /**
      * state change：
-     * QQ                                             IME                                       QQ
-     * TYPE_VIEW_FOCUSED                                                          autoclick
+     * TYPE_VIEW_FOCUSED
      * TYPE_WINDOW_STATE_CHANGED -> isChatWindow() -> VIEW_CLICK -> isPictureSendWindowNow() -> VIEW_CLICK -> isPictureSendDialog() -> autoclick
-     * state:                             1                                  2                                        3
+     * state:                                      1                                          2            3
      */
+    private int nowState = 0;
+    private String listenerName = null;
+
+    @Override
+    protected boolean onKeyEvent(KeyEvent event) {
+        Log.d(TAG, "onKeyEvent: " + event.getKeyCode());
+        switch (event.getKeyCode()) {
+            case KeyEvent.KEYCODE_BACK:
+            case KeyEvent.KEYCODE_HOME:
+            case KeyEvent.KEYCODE_MENU:
+                nowState = 0;
+        }
+        return false;
+    }
+
+
+
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-//        Log.d(TAG, "onAccessibilityEvent: " +
-//                AccessibilityEvent.eventTypeToString(event.getEventType()) + " " + event.getPackageName());
 //        if (event.getEventType() != AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED)
-//            Log.d(TAG, event.getPackageName() + "  " + AccessibilityEvent.eventTypeToString(event.getEventType())
-//                    + "   isChatWindow " + isChatWindowNow()
-//                    + "   isPictureSendWindowNow " + isPictureSendWindowNow()
-//                    + "   isPictureSendDialogNow " + isPictureSendDialogNow());
+            Log.d(TAG, event.getPackageName() + "  " + AccessibilityEvent.eventTypeToString(event.getEventType())
+                    + "   isChatWindow " + isChatWindowNow()
+                    + "   isPictureSendWindowNow " + isPictureSendWindowNow()
+                    + "   isPictureSendDialogNow " + isPictureSendDialogNow());
         switch (nowState) {
             case 0:
-                if (event.getPackageName().equals(QQ_PACKAGE_NAME)) {
-                    if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_FOCUSED
-                            || event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
-                        chaterName = isChatWindowNow();
-                        if (chaterName != null) {
-                            nowState = 1;
-
-                        } else {
-                            nowState = 0;
-                        }
-
+                if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_FOCUSED
+                        || event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+                    listenerName = isChatWindowNow();
+                    if (listenerName != null) {
+                        nowState = 1;
+                    } else {
+                        nowState = 0;
                     }
                 }
-                Log.d(TAG, "0 state: " + nowState);
+                Log.d(TAG, "0 state:" + nowState + "  " + listenerName);
                 break;
             case 1:
-                if (event.getPackageName().equals(IME_PACKAGE_NAME)) {
-                    if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_CLICKED) {
-                        if (isPictureSendWindowNow(chaterName)) {
-                            //开始执行自动发送操作
-                            nowState = 2;
-                        } else {
+                if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_FOCUSED
+                        || event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+                    if (isPictureSendWindowNow()) {
+                        nowState = 2;
+                    } else {
+                        if (isChatWindowNow() == null) {
                             nowState = 0;
                         }
-
-                        chaterName = null;
                     }
                 }
-                Log.d(TAG, "1 state: " + nowState);
+                Log.d(TAG, "1 state:" + nowState + "  " + listenerName);
                 break;
             case 2:
-                if (event.getPackageName().equals(QQ_PACKAGE_NAME)) {
-                    if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_CLICKED) {
-                        isChatWindowNow();
+                if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+                    boolean res = autoClickPersonWithName(listenerName, AccessibilityNodeInfo.ACTION_CLICK);
+                    if (res) {
+                        nowState = 3;
+                    } else {
                         nowState = 0;
-                        chaterName = null;
                     }
-
                 }
-                Log.d(TAG, "2 state: " + nowState);
+                Log.d(TAG, "2 state:" + nowState + "  " + listenerName);
+                break;
+            case 3:
+                if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+                    autoClickYesInSendDialog();
+                    nowState = 0;
+                    listenerName = null;
+                }
+                Log.d(TAG, "3 state:" + nowState + "  " + listenerName);
                 break;
         }
+        event.recycle();
     }
 
     @Override
@@ -110,6 +122,8 @@ public class AutoSendImageToQQ extends AccessibilityService {
         String chaterName = null;
         AccessibilityNodeInfo root = getRootInActiveWindow();
 
+        if (root == null) return null;
+
         List<AccessibilityNodeInfo> input = root.findAccessibilityNodeInfosByViewId(QQ_CHAT_INPUT_ID);
         List<AccessibilityNodeInfo> send = root.findAccessibilityNodeInfosByViewId(QQ_CHAT_SEND_ID);
         List<AccessibilityNodeInfo> chaterNames = null;
@@ -119,7 +133,7 @@ public class AutoSendImageToQQ extends AccessibilityService {
             chaterNames = root.findAccessibilityNodeInfosByViewId(QQ_CHATER_NAME_ID);
         } else if (input.size() == 1 && send.size() == 0) {
             //我的电脑
-            chaterNames = root.findAccessibilityNodeInfosByText(QQ_MY_PC_NAME_ID);
+            chaterNames = root.findAccessibilityNodeInfosByViewId(QQ_MY_PC_NAME_ID);
         }
         if (chaterNames != null && chaterNames.size() > 0) {
             chaterName = chaterNames.get(0).getText().toString();
@@ -134,23 +148,20 @@ public class AutoSendImageToQQ extends AccessibilityService {
     }
 
     /**
-     * 判断当前是否在表情发送联系人选择页面,如果是，找寻给定联系人名称，并点击
+     * 判断当前是否在表情发送联系人选择页面
      *
-     * @return 不在当前页面或未找到联系人返回false
+     * @return
      */
-    private boolean isPictureSendWindowNow(String chaterName) {
+    private boolean isPictureSendWindowNow() {
         boolean toReturn = false;
         AccessibilityNodeInfo root = getRootInActiveWindow();
 
+        if (root == null) return false;
+
         List<AccessibilityNodeInfo> search = root.findAccessibilityNodeInfosByViewId(QQ_SEARCH_INPUT_ID);
         List<AccessibilityNodeInfo> tabs = root.findAccessibilityNodeInfosByViewId(QQ_TABS_ID);
-        if (search.size() == 1 && tabs.size() == 0 && chaterName != null) {
-            List<AccessibilityNodeInfo> people = root.findAccessibilityNodeInfosByText(chaterName);
-            if (people.size() == 1) {
-                people.get(0).performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                toReturn = true;
-            }
-            clean(people);
+        if (search.size() == 1 && tabs.size() == 0) {
+            toReturn = true;
         }
 
         root.recycle();
@@ -162,20 +173,18 @@ public class AutoSendImageToQQ extends AccessibilityService {
 
 
     /**
-     * 判断当前是否为表情发送确认页面（对话框），是则点击确认按钮
+     * 判断当前是否为表情发送确认页面（对话框）
      *
-     * @return 点击了确认按钮为true，否则为false
+     * @return
      */
     private boolean isPictureSendDialogNow() {
         boolean toReturn = false;
         AccessibilityNodeInfo root = getRootInActiveWindow();
+        if (root == null) return false;
+
         List<AccessibilityNodeInfo> dialog = root.findAccessibilityNodeInfosByViewId(QQ_COMMIT_DIALOG_ID);
         if (dialog.size() == 1) {
-            List<AccessibilityNodeInfo> yes = root.findAccessibilityNodeInfosByViewId(QQ_COMMIT_DIALOG_YES_ID);
-            if (yes.size() == 1) {
-                toReturn = yes.get(0).performAction(AccessibilityNodeInfo.ACTION_CLICK);
-            }
-            clean(yes);
+            toReturn = true;
         }
 
         root.recycle();
@@ -185,11 +194,57 @@ public class AutoSendImageToQQ extends AccessibilityService {
     }
 
     /**
+     * 在发送列表中，点击具有给定名字的条目
+     *
+     * @param name   指定的text
+     * @param action 要执行的操作
+     * @return
+     */
+    private boolean autoClickPersonWithName(String name, int action) {
+        boolean toReturn = false;
+        AccessibilityNodeInfo root = getRootInActiveWindow();
+        if (root == null) return false;
+        List<AccessibilityNodeInfo> viewNode = root.findAccessibilityNodeInfosByText(name);
+        for (AccessibilityNodeInfo node : viewNode) {
+            if (node.getText().equals(name)) {
+                AccessibilityNodeInfo parent = node.getParent();
+                toReturn = parent.performAction(action);
+                parent.recycle();
+                Log.d(TAG, "autoClickPersonWithName: clicked: " + name);
+            }
+        }
+
+        clean(viewNode);
+        root.recycle();
+
+        return toReturn;
+    }
+
+    /**
+     * 自动点击发送确认对话框yes按钮
+     *
+     * @return
+     */
+    private boolean autoClickYesInSendDialog() {
+        boolean toReturn = false;
+        AccessibilityNodeInfo root = getRootInActiveWindow();
+        if (root == null) return false;
+        List<AccessibilityNodeInfo> yes = root.findAccessibilityNodeInfosByViewId(QQ_COMMIT_DIALOG_YES_ID);
+        if (yes.size() == 1) {
+            toReturn = yes.get(0).performAction(AccessibilityNodeInfo.ACTION_CLICK);
+        }
+        root.recycle();
+        clean(yes);
+        return toReturn;
+    }
+
+    /**
      * recycle()
      *
      * @param nodes
      */
     private void clean(List<AccessibilityNodeInfo> nodes) {
+        if (nodes == null) return;
         for (AccessibilityNodeInfo node : nodes) {
             node.recycle();
         }
