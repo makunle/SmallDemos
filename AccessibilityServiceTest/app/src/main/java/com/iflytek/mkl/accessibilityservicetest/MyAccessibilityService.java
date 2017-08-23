6b13a7e13a5f8dc66c689bc4fda0b94d2fe6dc1a
makunle <makunle@qq.com>
2017-08-23 15:29:23 +0800

    with dispatchGesture

--- AccessibilityServiceTest/app/src/main/java/com/iflytek/mkl/accessibilityservicetest/MyAccessibilityService.java
+++ AccessibilityServiceTest/app/src/main/java/com/iflytek/mkl/accessibilityservicetest/MyAccessibilityService.java
@@ -2,7 +2,9 @@ package com.iflytek.mkl.accessibilityservicetest;
 
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
+import android.accessibilityservice.GestureDescription;
 import android.content.Intent;
+import android.graphics.Path;
 import android.os.Build;
 import android.os.IBinder;
 import android.support.annotation.RequiresApi;
@@ -27,19 +29,17 @@ public class MyAccessibilityService extends AccessibilityService {
 
     @Override
     protected boolean onKeyEvent(KeyEvent event) {
-        Util.show("KEY: " + event.getKeyCode() + " " + event.getAction());
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-            if (event.getKeyCode() == 24) {
+            if (event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_UP) {
                 MagnificationController controller = getMagnificationController();
                 controller.setScale(controller.getScale() * 1.1f, true);
                 return true;
             }
-            if (event.getKeyCode() == 25) {
+            if (event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_DOWN) {
                 MagnificationController controller = getMagnificationController();
                 controller.setScale(controller.getScale() * 0.9f, true);
                 return true;
             }
-
         }
         return false;
     }
@@ -60,6 +60,28 @@ public class MyAccessibilityService extends AccessibilityService {
             case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
                 Util.show(AccessibilityEvent.eventTypeToString(eventType) + " " + event.getText() + "   pre window");
                 Util.autoInputCode(event, getRootInActiveWindow(), this);
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                    GestureDescription.Builder builder = new GestureDescription.Builder();
+                    Path path = new Path();
+                    path.moveTo(400, 400);
+                    path.lineTo(400, 1000);
+                    builder.addStroke(new GestureDescription.StrokeDescription(path, 500, 500));
+                    GestureDescription ges = builder.build();
+                    dispatchGesture(ges, new GestureResultCallback() {
+                        @Override
+                        public void onCompleted(GestureDescription gestureDescription) {
+                            super.onCompleted(gestureDescription);
+                            Util.show("gesture completed");
+                        }
+
+                        @Override
+                        public void onCancelled(GestureDescription gestureDescription) {
+                            super.onCancelled(gestureDescription);
+                            Util.show("gesture cancelled");
+                        }
+                    }, null);
+                }
                 break;
             case AccessibilityEvent.TYPE_VIEW_CLICKED:
                 Util.show("VIEW_CLICKED " + event.getClassName() + " " + event.getText());
@@ -107,7 +129,17 @@ public class MyAccessibilityService extends AccessibilityService {
                 disableSelf();
             }
         }
-        return super.onGesture(gestureId);
+        else if(gestureId == GESTURE_SWIPE_LEFT_AND_RIGHT) {
+
+        }
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            AccessibilityServiceInfo info = getServiceInfo();
+            info.notificationTimeout = 500;
+            setServiceInfo(info);
+        }
+
+        return false;
     }
 
     @Override
