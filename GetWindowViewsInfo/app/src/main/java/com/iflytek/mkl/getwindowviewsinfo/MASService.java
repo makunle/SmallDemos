71e52f411f9535238745d2759b6cff05997bf6e3
makunle <makunle@qq.com>
2017-09-06 20:17:20 +0800

    add write views info to .log file

--- GetWindowViewsInfo/app/src/main/java/com/iflytek/mkl/getwindowviewsinfo/MASService.java
+++ GetWindowViewsInfo/app/src/main/java/com/iflytek/mkl/getwindowviewsinfo/MASService.java
@@ -2,6 +2,8 @@ package com.iflytek.mkl.getwindowviewsinfo;
 
 import android.accessibilityservice.AccessibilityService;
 import android.os.Build;
+import android.os.Environment;
+import android.provider.ContactsContract;
 import android.support.annotation.RequiresApi;
 import android.util.Log;
 import android.util.StringBuilderPrinter;
@@ -10,6 +12,18 @@ import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.view.accessibility.AccessibilityWindowInfo;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 /**
  * Created by makunle on 2017/8/31.
  */
@@ -17,6 +31,8 @@ import android.view.accessibility.AccessibilityWindowInfo;
 public class MASService extends AccessibilityService {
 
     private static final String TAG = "ViewTools";
+
+    private static final String LOG_FILE = "viewinfo.log";
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
@@ -28,19 +44,26 @@ public class MASService extends AccessibilityService {
         if (event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_UP) {
             if (event.getAction() == KeyEvent.ACTION_DOWN) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    SimpleDateFormat format = new SimpleDateFormat("  HH:mm:ss");
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                         for (AccessibilityWindowInfo info : getWindows()) {
                             AccessibilityNodeInfo root = info.getRoot();
-                            show("========================" + root.getPackageName() + "========================");
+                            show("======================== " + root.getPackageName() + format.format(Calendar.getInstance().getTime()) + " ========================");
                             show(root, 0);
                         }
                     } else {
-                        show("========================" + getPackageName() + "========================");
+                        show("======================== " + getPackageName() + format.format(Calendar.getInstance().getTime()) + " ========================");
                         show(getRootInActiveWindow(), 0);
                     }
+                    show("======================================================\n");
                 }
             }
             return true;
+        } else if (event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                File file = new File(Environment.getExternalStorageDirectory() + File.separator + LOG_FILE);
+                file.delete();
+            }
         }
         return false;
     }
@@ -52,6 +75,18 @@ public class MASService extends AccessibilityService {
 
     private void show(String str) {
         Log.d(TAG, str);
+        File file = new File(Environment.getExternalStorageDirectory() + File.separator + LOG_FILE);
+        try {
+            FileOutputStream fs = new FileOutputStream(file, true);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fs));
+            bw.write(str + "\n");
+            bw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     private void show(AccessibilityNodeInfo root, int level) {
@@ -61,10 +96,13 @@ public class MASService extends AccessibilityService {
             sb.append("    ");
         }
         if (level > 0) sb.append("┗");
-        sb.append("class:").append(root.getClassName());
-        sb.append(" text:").append(root.getText());
+
+        String[] classSplit = root.getClassName().toString().split(".");
+//        sb.append(classSplit.length > 0 ? classSplit[classSplit.length - 1] : "null");
+        sb.append(root.getClassName());
+        sb.append(" ").append(root.getText());
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-            sb.append(" id:").append(root.getViewIdResourceName());
+            sb.append(" ").append(root.getViewIdResourceName());
         }
         show(sb.toString());
         for (int i = 0; i < root.getChildCount(); i++) {
