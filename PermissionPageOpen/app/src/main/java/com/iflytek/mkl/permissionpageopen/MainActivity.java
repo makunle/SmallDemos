8774d6916b5a3cf5a7c5ded0ff725253df4ce5b3
makunle <makunle@qq.com>
2017-09-15 17:16:23 +0800

    a

--- PermissionPageOpen/app/src/main/java/com/iflytek/mkl/permissionpageopen/MainActivity.java
+++ PermissionPageOpen/app/src/main/java/com/iflytek/mkl/permissionpageopen/MainActivity.java
@@ -5,6 +5,7 @@ import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +18,16 @@ public class MainActivity extends AppCompatActivity {
     public void PACKAGE_USAGE_STATS_page(View view) {
         Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
         startActivity(intent);
+    }
+
+    public void changeViewSize(View view) {
+        View root = findViewById(R.id.root);
+        ViewGroup.LayoutParams params = root.getLayoutParams();
+        if(params.height > 800){
+            params.height = 700;
+        }else{
+            params.height = 900;
+        }
+        root.setLayoutParams(params);
     }
 }
