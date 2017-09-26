454d0164cf4267295741b7a8cbb13ce6aea0c56e
makunle <makunle@qq.com>
2017-09-26 17:16:52 +0800

    add jump to browser

--- AlertWindowDemo/app/src/main/java/com/iflytek/mkl/alertwindowdemo/HalfWindowAlertActivity.java
+++ AlertWindowDemo/app/src/main/java/com/iflytek/mkl/alertwindowdemo/HalfWindowAlertActivity.java
@@ -1,7 +1,10 @@
 package com.iflytek.mkl.alertwindowdemo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HalfWindowAlertActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class HalfWindowAlertActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_half_window_alert);
+        findViewById(R.id.float_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://www.baidu.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
     }
 }
